1ec0ded7412efa8df5edcfbe44a210681840df98
burninghering <37091602+burninghering@users.noreply.github.com>
2020-03-28 22:39:22 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,52 @@
+// console.log('app.js');
+
+// console.log(1);
+
+// var count =1;
+// console.log(count);
+
+// var isMan = true; //Bool
+// var isMan = "true"; //String'
+// console.log(isMan, true);
+
+
+// function log(str) {
+//     console.log(str);
+//   }
+//   log('hello');
+
+// var image = {
+// width:100,
+// height:100,
+// name='1.png'
+// };
+// var imageWidth=100;
+// var ImageName='1.png'
+// console.log(image);
+
+var image1=[1,2,3,4,5,6,'a',{}];
+image1[0]='a'; //배열 1번을 a로 바꾸는 것
+
+
+var data=null; //값이 아예 없는 것
+var data1=''; //빈 값
+var data2;
+console.log(data,data1,data2);
+
+console.log(1+2);
+console.log('a'+'b');
+console.log(1+'4');
+var count=1;
+var number='2';
+console.log(count+number);
+
+console.log(1==1); //true
+console.log(0 == '', 0 === ''); //항상 === 3개 쓰는 것 기억
+
+if(true){
+    console.log(1==1);
+}
+
+
+console.log(typeof.1);
+console.log(typeof []);
