0899fa08a3a484b0463da0f2712cad40825513db
burninghering <37091602+burninghering@users.noreply.github.com>
2020-04-12 20:23:17 +0900

    Create app.js

---
+++ 02/app.js
@@ -0,0 +1,15 @@
+console.log('app');
+
+
+// Create (기사 목록에 기사 하나 끼워넣는것처럼)
+var todos = ['운동'];
+var todo = '게임';
+todos.push(todo);
+console.log(todos);
+
+// Read
+
+todos.forEach(function(todo){
+
+console.log(todo);
+});
