347a9f04e6d12a7e1cdcbbce4e55c9aed276e0a9
burninghering <37091602+burninghering@users.noreply.github.com>
2020-04-15 03:42:29 +0900

    Create app.js

---
+++ 03/app.js
@@ -0,0 +1,24 @@
+// console.log(window.outerWidth);
+// console.log(window.name);
+// //window.open('http://www.daum.net');
+// //mdn 사이트 참고하면 좋음!
+
+// //location.href='http://www.daum.net';
+// //location.reload;
+
+// history.back();
+
+// history.go(1);
+// history.forward;
+
+// screen.availWidth;
+
+// navigator.userAgent;
+
+
+var debug =document.getElementById('debug');
+console.log(debug);
+
+// var box=document.querySelector('.box');
+var box2=document.querySelectorAll('.box');
+console.log(box2);
